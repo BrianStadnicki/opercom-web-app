ac52b269f42fc83cdbebe1728a295a7dc1d129dd
Brian Stadnicki <stadnickibrian@gmail.com>
2022-10-08 18:54:50 +0100

    teams sidebar sort channels alphabetically

--- src/lib/sidebar/Team.svelte
+++ src/lib/sidebar/Team.svelte
@@ -9,7 +9,8 @@
 
 <details class="team">
     <summary>{team.name}</summary>
-    {#each team.channels as channel}
+    {#each team.channels.sort((a, b) => a.id === team.id ? -1 : a.name.localeCompare(b.name))
+            as channel}
         <Channel channel={channel} on:message active={activeChannel === channel.id}></Channel>
     {/each}
 </details>
