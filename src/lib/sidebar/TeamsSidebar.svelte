08004e5fbca3d8c5ed46d57fce80c90d14e2a89c
Brian Stadnicki <stadnickibrian@gmail.com>
2022-10-14 23:13:41 +0100

    sidebar show main and hidden teams Fixes #13

--- src/lib/sidebar/TeamsSidebar.svelte
+++ src/lib/sidebar/TeamsSidebar.svelte
@@ -13,13 +13,19 @@
 
         dispatch('message', event.detail);
     }
-
 </script>
 
 <div>
-    {#each teams as team}
+    {#each teams.filter(team => team.isFavorite) as team}
         <Team team={team} activeChannel={activeChannel} on:message={handleEvent}></Team>
     {/each}
+    <details>
+        <summary>Hidden</summary>
+        {#each teams.filter(team => !team.isFavorite && !team.isArchived) as team}
+            <Team team={team} activeChannel={activeChannel} on:message={handleEvent}></Team>
+        {/each}
+    </details>
+
 </div>
 
 <style lang="scss">
@@ -28,5 +34,9 @@
     overflow-y: auto;
     background-color: $colour-1;
     scroll-behavior: smooth;
+  }
+
+  summary {
+    background-color: orange;
   }
 </style>
